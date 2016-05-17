00a3b336231f3bf0d52f5d9d551a3dc2518e11cf
Cillas <cillassubira@gmail.com>
2016-05-17 10:09:47 +0200

    Fix relationships with Requests and Shifts on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :shifts
+	has_many :requests
 end
