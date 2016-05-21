63286423bf72027716b7f070313c6dfe8f737a4e
Cillas <cillassubira@gmail.com>
2016-05-22 01:47:08 +0200

    Add notification of received requests to navbar

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,9 +2,11 @@ class ShiftsController < ApplicationController
 	before_action :authenticate_user! 
 	def index
 	  @shifts = User.find(current_user.id).shifts
+	  @requests_received = Request.where(user_answering_id: params[:user_id], status: 1)
 	end
 
 	def show
+
 		@shift_to_be_changed = Shift.find(params[:id])
 		@available_shifts = Shift.calculate_free_shifts(@shift_to_be_changed)
 		@available_requests = Shift.available_shifts_to_return(@available_shifts, @shift_to_be_changed)
