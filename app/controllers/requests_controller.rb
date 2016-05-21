1f67925c384fc94cd2bf8d1889386007f6d48d6e
Cillas <cillassubira@gmail.com>
2016-05-22 01:19:42 +0200

    Fix request accept and reject features

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,58 +1,67 @@
 class RequestsController < ApplicationController
 	def inbox
-		@user_id = params[:user_id]
 		@requests_sent = Request.where(user_requesting_id: params[:user_id], status: 1)
 		@requests_accepted_by_others = Request.where(user_requesting_id: params[:user_id], status: 2)
 		@requests_received = Request.where(user_answering_id: params[:user_id], status: 1)
-	end
-
-
-	def accept
-		request = Request.find(params[:id])
-		current_shift_user = Shift.find(request.shift_answering_id)
-		#current_shift_collegue = Shift.find
-		new_shift_user = Shift.find(request.shift_requested_id)
-		#new_shift_collegue = 
-		collegue_id = new_shift_user.user_id
-		current_shift_user.update(user_id: collegue_id)
-
-		new_shift_user.update(user_id: current_user.id)
-		#falta cambiar los libress
-
-
-		request.update(status: 2)
-		#otras peticiones mismo turno
-		other_requests = Request.where(shift_requested_id: new_shift_user, user_requesting_id: collegue_id)
-		if !other_requests.empty?
-			other_requests.update(status: 3)
-		end
-
-
 
 	end
 
 	def create
-		# binding.pry
-		@user_requesting_id = params[:user_id]
-		@user_answering_id = Shift.find(params[:shift_answering_id]).user_id
-		@shift_requested_id = params[:shift_id]
-		@shift_answering_id = params[:shift_answering_id]
-		hello = Request.create(
-			user_requesting_id: @user_requesting_id, 
-			user_answering_id: @user_answering_id,
-			shift_requested_id: @shift_requested_id,
-			shift_answering_id: @shift_answering_id,
+		request = Request.new(
+			user_requesting_id: params[:user_id], 
+			user_answering_id: Shift.find(params[:shift_answering_id]).user_id,
+			shift_requested_id: params[:shift_id],
+			shift_answering_id: params[:shift_answering_id],
 			status: 1
-			)
-		respond_to do |format|
-			format.js{}
-		end
+		)
+		if request.save
+      flash[:success] = "Your request has been sent!"
+      #redirect_to @user
+    else
+      #render 'new'
+    end
+		# respond_to do |format|
+		# 	format.js{}
+		# end
 	end
 
+	def accept
+		request = Request.find(params[:id])
 
+		current_user_shift = Shift.find(request.shift_answering_id)
+		collegue_shift = Shift.find(request.shift_requested_id)
+		collegue_id = collegue_shift.user_id
+		#free corresponding shifts(gaps)
+		current_user_gap = Shift.find_by(user_id: current_user.id, day: collegue_shift.day)
+		collegue_gap = Shift.find_by(user_id: collegue_id, day: current_user_shift.day)
+		#interchanging free shifts(gaps)
+		current_user_gap.update(user_id: collegue_id)
+		collegue_gap.update(user_id: current_user.id)
+		#interchanging Shifts (according to request)
+		current_user_shift.update(user_id: collegue_id)
+		collegue_shift.update(user_id: current_user.id)
+		#updating request to accepted status
+		request.update(status: 2)
+		#cancelling other requests of same worker for the same shift
+		other_requests = Request.where(shift_requested_id: request.shift_requested_id, user_requesting_id: collegue_id, status: 1)
+		if !other_requests.empty?
+			other_requests.update_all(status: 3)
+		end
+		redirect_to requests_inbox_path
+	end
 
-	private
+	def reject
+		request = Request.find(params[:id])
+		request.update(status: 3)
+		redirect_to requests_inbox_path
+	end
 
+	def mark_as_seen
+		request = Request.find(params[:id])
+		request.update(status: 4)
+		redirect_to requests_inbox_path
+
+	end
 
 
 end
--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,26 +1,22 @@
 class ShiftsController < ApplicationController
 	before_action :authenticate_user! 
 	def index
-	  @user = User.find(current_user.id)
-	  @shifts = @user.shifts
+	  @shifts = User.find(current_user.id).shifts
 	end
 
 	def show
-		@user_id = current_user.id
 		@shift_to_be_changed = Shift.find(params[:id])
 		@available_shifts = Shift.calculate_free_shifts(@shift_to_be_changed)
 		@available_requests = Shift.available_shifts_to_return(@available_shifts, @shift_to_be_changed)
-
 	end
 
-
-	def other
-		@user = User.find(params[:user_id])
-		@shift = Shift.find(params[:id])
-		render json: @shift
+	def shift_info
+		shift = Shift.find(params[:id])
+		render json: shift
+		# respond_to do |format|
+		# 	format.js
+		# 	format.html
+		# end
 	end
-
-
-	private
 
 end
